dfa7c7118741c35322f8645fd2ae0850cd9435bb
daifiyum <daiqisen131@gmail.com>
2024-06-25 11:38:27 +0800

    Optimize DLL calls

--- utils/toast.go
+++ utils/toast.go
@@ -7,33 +7,13 @@ import (
 	"unsafe"
 )
 
-// 加载 DLL 并查找指定的函数, toast.dll采用rust基于win api编写
-func loadToastDLL(procName string) (*syscall.DLL, *syscall.Proc, error) {
-	// 加载库
-	lib, err := syscall.LoadDLL("./resources/libs/toast.dll")
-	if err != nil {
-		return nil, nil, fmt.Errorf("error loading DLL: %v", err)
-	}
+var (
+	toast        = syscall.NewLazyDLL("./resources/libs/toast.dll")
+	toastProc    = toast.NewProc("toast")
+	registerProc = toast.NewProc("register_toast_notification")
+)
 
-	// 查找函数
-	proc, err := lib.FindProc(procName)
-	if err != nil {
-		lib.Release()
-		return nil, nil, fmt.Errorf("error finding %s function: %v", procName, err)
-	}
-
-	return lib, proc, nil
-}
-
-// ShowToast 显示一个带有给定标题和消息的 toast 通知
 func ShowToast(aumid, title, message string) error {
-	lib, toastProc, err := loadToastDLL("toast")
-	if err != nil {
-		return err
-	}
-	defer lib.Release()
-
-	// 准备输入参数
 	cAumid, err := syscall.BytePtrFromString(aumid)
 	if err != nil {
 		return fmt.Errorf("error converting title to C string: %v", err)
@@ -47,7 +27,6 @@ func ShowToast(aumid, title, message string) error {
 		return fmt.Errorf("error converting message to C string: %v", err)
 	}
 
-	// 调用函数
 	ret, _, _ := toastProc.Call(uintptr(unsafe.Pointer(cAumid)), uintptr(unsafe.Pointer(cTitle)), uintptr(unsafe.Pointer(cMessage)))
 	if ret == 0 {
 		return fmt.Errorf("failed to show toast")
@@ -58,13 +37,6 @@ func ShowToast(aumid, title, message string) error {
 
 // HKEY_CURRENT_USER\Software\Classes\AppUserModelId\<id>
 func RegisterToast(id, name, path string) error {
-	lib, registerProc, err := loadToastDLL("register_toast_notification")
-	if err != nil {
-		return err
-	}
-	defer lib.Release()
-
-	// 准备输入参数
 	cId, err := syscall.BytePtrFromString(id)
 	if err != nil {
 		return fmt.Errorf("error converting title to C string: %v", err)
@@ -78,7 +50,6 @@ func RegisterToast(id, name, path string) error {
 		return fmt.Errorf("error converting message to C string: %v", err)
 	}
 
-	// 调用函数
 	ret, _, _ := registerProc.Call(uintptr(unsafe.Pointer(cId)), uintptr(unsafe.Pointer(cName)), uintptr(unsafe.Pointer(cPath)))
 	if ret == 0 {
 		return fmt.Errorf("failed to register toast")
@@ -87,7 +58,7 @@ func RegisterToast(id, name, path string) error {
 	return nil
 }
 
-// 初始化，注册通知ID, 以显示通知图标和名称
+// APPID 注册 以显示通知图标和名称
 func init() {
 	toastIcon, err := filepath.Abs("./resources/icons/box.ico")
 	if err != nil {
